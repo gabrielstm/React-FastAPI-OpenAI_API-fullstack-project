e97f63be878994ac17f4ac9b6b748fb6c1fce10e
gabrielzitto <gos2@cin.ufpe.br>
2025-10-09 23:02:14 -0300

    init

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "http://localhost:8000/api"
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://fast-api-2-i5t4.onrender.com/api"
+export const API_BASE_URL = "http://localhost:8000/api"
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://fast-api-2-i5t4.onrender.com/api"
